220aa33e3c8453d0edf6733fc00b9f47d65405fc
Sukhada Gholba <sukhadagholba@gmail.com>
2019-01-22 16:59:50 -0800

    updated route

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ app.prepare()
 .then(() => {
   const server = express()
 
+
   server.get('/p/:id', (req, res) => {
     const actualPage = '/post'
-    const queryParams = { title: req.params.id } 
+    const queryParams = { id: req.params.id }
     app.render(req, res, actualPage, queryParams)
   })
 
